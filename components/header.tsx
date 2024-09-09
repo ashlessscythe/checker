6b410a67bc0871f6e59b7faddeef1d8e0b7b9ca9
Ashless Scythe <drakesav@gmail.com>
2024-09-09 21:35:56 +0000

    gray button

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,10 @@
-'use client'
-import Link from 'next/link';
-import { useAuth } from '../hooks/authContext';
+"use client";
+import Link from "next/link";
+import { useAuth } from "../hooks/authContext";
 
 interface HeaderProps {
-    setIsAuthModalOpen: (isOpen: boolean) => void;
-  };
+  setIsAuthModalOpen: (isOpen: boolean) => void;
+}
 
 export default function Header({ setIsAuthModalOpen }: HeaderProps) {
   const { isAuthenticated, isAdmin, isAuthorized, user, signOut } = useAuth();
@@ -12,7 +12,7 @@ export default function Header({ setIsAuthModalOpen }: HeaderProps) {
   const handleLogout = async () => {
     await signOut();
     // You might want to redirect the user or update the UI after logout
-    window.location.href = '/'; // Redirect to home after logout
+    window.location.href = "/"; // Redirect to home after logout
   };
 
   return (
@@ -23,18 +23,24 @@ export default function Header({ setIsAuthModalOpen }: HeaderProps) {
           <div className="flex items-center space-x-4">
             <span>Welcome, {user?.email}</span>
             {isAdmin && (
-              <Link href="/admin-page" className="text-blue-500 hover:text-blue-700">
+              <Link
+                href="/admin-page"
+                className="text-blue-500 hover:text-blue-700"
+              >
                 Admin Panel
               </Link>
             )}
             {isAuthorized && (
-              <Link href="/checklist" className="text-green-500 hover:text-green-700">
+              <Link
+                href="/checklist"
+                className="text-green-500 hover:text-green-700"
+              >
                 Checklist
               </Link>
             )}
-            <button 
+            <button
               onClick={handleLogout}
-              className="bg-red-500 text-white p-2 rounded hover:bg-red-600 transition-colors" 
+              className="bg-red-500 text-white p-2 rounded hover:bg-red-600 transition-colors"
             >
               Log Out
             </button>
@@ -42,7 +48,7 @@ export default function Header({ setIsAuthModalOpen }: HeaderProps) {
         ) : (
           <button
             onClick={() => setIsAuthModalOpen(true)}
-            className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600 transition-colors"
+            className="bg-gray-500 text-white p-2 rounded hover:bg-blue-600 transition-colors"
           >
             Log In
           </button>
